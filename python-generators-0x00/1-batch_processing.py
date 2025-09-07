5c80e8302c297ba489f6d95663832b2d27e96fc9
eelmishtawy <eslamelmishtawy@gmail.com>
2025-09-07 21:32:23 +0300

    update 2

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -11,7 +11,7 @@ def stream_users_in_batches(batch_size):
 	cursor = conn.cursor(dictionary=True)
 	offset = 0
 	while True:
-		cursor.execute(f"SELECT * FROM [user_data] LIMIT %s OFFSET %s", (batch_size, offset))
+		cursor.execute("SELECT * FROM user_data LIMIT %s OFFSET %s", (batch_size, offset))
 		batch = cursor.fetchall()
 		if not batch:
 			break

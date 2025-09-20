ff0d21e51eaa2b644a5dff0a9d4c4335124dcc0a
eelmishtawy <eslamelmishtawy@gmail.com>
2025-09-21 00:41:41 +0300

    test client

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,26 @@
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+
+from client import GithubOrgClient
+
+class TestGithubOrgClient(unittest.TestCase):
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch("client.get_json")
+    def test_org(self, org_name, mock_get_json):
+        expected_payload = {"org": org_name, "payload": True}
+        mock_get_json.return_value = expected_payload
+
+        client = GithubOrgClient(org_name)
+        result = client.org
+
+        self.assertEqual(result, expected_payload)
+        mock_get_json.assert_called_once_with(
+            GithubOrgClient.ORG_URL.format(org=org_name)
+        )
+
+if __name__ == "__main__":
+    unittest.main()
